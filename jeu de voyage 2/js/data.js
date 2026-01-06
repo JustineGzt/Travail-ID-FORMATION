1da57b8c5674b38ef180a4596a3b8c9c42f2fedc
TonNom <gozetjustine@gmail.com>
2026-01-06 12:13:10 +0100

    Mise à jour

--- jeu de voyage 2/js/data.js
+++
@@ -1,5 +0,0 @@
-// Contenu des cartes
-const cards = [
-  { type: "question", content: "Quelle est ta couleur préférée ?" },
-  { type: "devinette", content: "Je suis petit, j'ai des ailes, qui suis-je ?" }
-];
